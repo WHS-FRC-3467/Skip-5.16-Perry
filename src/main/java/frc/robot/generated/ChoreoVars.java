1a8045ca23d5d3cb2497ad63ccfccebf3948f40d
Driver3467 <frc_driver_3467@team3467.org>
2026-03-22 14:51:17 -0400

    Fix formatting

--- src/main/java/frc/robot/generated/ChoreoVars.java
+++ src/main/java/frc/robot/generated/ChoreoVars.java
@@ -1,22 +1,23 @@
 // spotless:off
 package frc.robot.generated;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.*;
 
 /**
- * Generated file containing variables defined in Choreo.
- * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
- * in the Choreo GUI.
+ * Generated file containing variables defined in Choreo. DO NOT MODIFY THIS FILE YOURSELF; instead,
+ * change these values in the Choreo GUI.
  */
 public final class ChoreoVars {
 
-
     public static final class Poses {
-        public static final Pose2d NeutralShoot = new Pose2d(3.12514, 7.3089199, Rotation2d.fromRadians(-1.1536837));
-        public static final Pose2d TunnelEntrance = new Pose2d(4, 7.3992772, Rotation2d.fromRadians(-1.5707963));
-        public static final Pose2d TunnelExit = new Pose2d(5.65, 7.3992772, Rotation2d.fromRadians(-1.5707963));
+        public static final Pose2d NeutralShoot =
+                new Pose2d(3.12514, 7.3089199, Rotation2d.fromRadians(-1.1536837));
+        public static final Pose2d TunnelEntrance =
+                new Pose2d(4, 7.3992772, Rotation2d.fromRadians(-1.5707963));
+        public static final Pose2d TunnelExit =
+                new Pose2d(5.65, 7.3992772, Rotation2d.fromRadians(-1.5707963));
     }
 }
 // spotless:on
