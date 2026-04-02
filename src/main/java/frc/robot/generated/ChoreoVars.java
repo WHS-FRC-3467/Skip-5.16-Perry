d54635a500f652bebcd780f9d4aafe295d489bf1
cabnit-rs <jkistler28@windhamsd.org>
2026-04-02 09:01:20 -0400

    merge conflicts part two

--- src/main/java/frc/robot/generated/ChoreoVars.java
+++ src/main/java/frc/robot/generated/ChoreoVars.java
@@ -1,13 +1,14 @@
+// spotless:off
 package frc.robot.generated;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.*;
 
 /**
- * Generated file containing variables defined in Choreo. DO NOT MODIFY THIS FILE YOURSELF; instead,
- * change these values in the Choreo GUI.
+ * Generated file containing variables defined in Choreo.
+ * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
+ * in the Choreo GUI.
  */
 public final class ChoreoVars {
     public static final Distance Neutral2ML_Safe_X1 = Units.Meters.of(5.95);
@@ -15,37 +16,11 @@ public final class ChoreoVars {
     public static final Distance Neutral2ML_Safe_Y = Units.Meters.of(4.5);
 
     public static final class Poses {
-        public static final Pose2d BumpEntrance =
-                new Pose2d(5.6645226, 5.7927642, Rotation2d.fromRadians(6.2831853));
-        public static final Pose2d BumpExit =
-                new Pose2d(3.4681101, 5.7927642, Rotation2d.fromRadians(6.2831853));
-        public static final Pose2d DecisionPose =
-                new Pose2d(6.7, 7.3992772, Rotation2d.fromRadians(1.5707963));
-        public static final Pose2d DumperShot =
-                new Pose2d(3.0666702, 5.47686, Rotation2d.fromRadians(-0.8028515));
-        public static final Pose2d LanePose1ML =
-                new Pose2d(5.95, 6.4, Rotation2d.fromRadians(1.5707963));
-        public static final Pose2d LanePose2ML =
-                new Pose2d(6.7, 6.4, Rotation2d.fromRadians(1.5707963));
-        public static final Pose2d LanePose3ML =
-                new Pose2d(7.8, 6.4, Rotation2d.fromRadians(1.5707963));
-        public static final Pose2d NeutralShoot =
-                new Pose2d(3.12514, 7.3089199, Rotation2d.fromRadians(-1.1536837));
-        public static final Pose2d TunnelEntrance =
-                new Pose2d(4, 7.3992772, Rotation2d.fromRadians(-1.5707963));
-        public static final Pose2d TunnelExit =
-                new Pose2d(5.65, 7.3992772, Rotation2d.fromRadians(-1.5707963));
-        public static final Pose2d DecisionPose = new Pose2d(6.7, 7.399, Rotation2d.fromRadians(1.571));
-        public static final Pose2d DumperShot = new Pose2d(3.067, 5.477, Rotation2d.fromRadians(-0.803));
-        public static final Pose2d LanePose1ML = new Pose2d(5.95, 6.4, Rotation2d.fromRadians(1.571));
-        public static final Pose2d LanePose2ML = new Pose2d(6.7, 6.4, Rotation2d.fromRadians(1.571));
-        public static final Pose2d LanePose3ML = new Pose2d(7.8, 6.4, Rotation2d.fromRadians(1.571));
-        public static final Pose2d NeutralShoot = new Pose2d(3.125, 7.309, Rotation2d.fromRadians(-1.154));
-        public static final Pose2d TunnelEntrance = new Pose2d(4, 7.399, Rotation2d.fromRadians(-1.571));
-        public static final Pose2d TunnelExit = new Pose2d(5.65, 7.399, Rotation2d.fromRadians(-1.571));
-
-        private Poses() {}
+        public static final Pose2d BumpEntrance = new Pose2d(5.6645226, 5.7927642, Rotation2d.fromRadians(6.2831853));
+        public static final Pose2d BumpExit = new Pose2d(3.4681101, 5.7927642, Rotation2d.fromRadians(6.2831853));
+        public static final Pose2d NeutralShoot = new Pose2d(3.12514, 7.3089199, Rotation2d.fromRadians(-1.1536837));
+        public static final Pose2d TunnelEntrance = new Pose2d(4, 7.3992772, Rotation2d.fromRadians(-1.5707963));
+        public static final Pose2d TunnelExit = new Pose2d(5.65, 7.3992772, Rotation2d.fromRadians(-1.5707963));
     }
-
-    private ChoreoVars() {}
 }
+// spotless:on
