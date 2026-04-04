40aa9b2873897e23e2d563708a62b5711b0a80d8
frc3467 <frc3467@frc3467s-Mac-mini.local>
2026-04-04 10:08:55 -0400

    Update at 'Sat Apr 04 10:08:55 EDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,7 +30,7 @@ import java.util.Set;
  * (log replay from a file).
  */
 public final class Constants {
-    public static final Mode simMode = Mode.REPLAY;
+    public static final Mode simMode = Mode.SIM;
     public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 
     public static final boolean tuningMode = false;
