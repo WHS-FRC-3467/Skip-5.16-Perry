7b9c48aa0c951af80969fed698e97cfa0cf8ade9
Noah-H3467 <nhaskell26@team3467.org>
2026-04-11 13:40:04 -0400

    Log mechanism linear pos/velocity on change (tolerance=0.005)

--- src/main/java/frc/lib/mechanisms/Mechanism.java
+++ src/main/java/frc/lib/mechanisms/Mechanism.java
@@ -34,6 +34,7 @@ import edu.wpi.first.units.measure.Voltage;
 import frc.lib.io.motor.MotorIO;
 import frc.lib.io.motor.MotorIO.PIDSlot;
 import frc.lib.io.motor.MotorInputsAutoLogged;
+import frc.lib.util.LoggedDouble;
 import frc.lib.util.LoggedTunableNumber;
 import frc.lib.util.PID;
 
@@ -56,13 +57,19 @@ public abstract class Mechanism<T extends MotorIO> {
     @Getter protected final String name;
     protected final MotorInputsAutoLogged inputs = new MotorInputsAutoLogged();
     protected final T io;
-    private final List<TunablePidConfig> tunablePidConfigs = new ArrayList<>();
+    private final List<TunablePidConfig> tunablePidConfigs = new ArrayList<>(2);
 
     /**
      * Effective radius used for linear-to-angular conversion (meters). {@code null} means linear
      * control is not configured for this mechanism.
      */
     private Double radiusMeters = null;
+
+    // Logged linear values (created when radius is configured)
+    private LoggedDouble loggedLinearPosition = null;
+    private LoggedDouble loggedLinearPositionError = null;
+    private LoggedDouble loggedLinearVelocity = null;
+    private LoggedDouble loggedLinearVelocityError = null;
 
     protected Mechanism(String name, T io) {
         this.name = name;
@@ -84,6 +91,11 @@ public abstract class Mechanism<T extends MotorIO> {
             throw new IllegalArgumentException("radius must be greater than 0 meters");
         }
         this.radiusMeters = r;
+        // initialize logged outputs for linear equivalents
+        this.loggedLinearPosition = new LoggedDouble(this.name + "/LinearPosition", 0.005);
+        this.loggedLinearPositionError = new LoggedDouble(this.name + "/LinearPositionError", 0.005);
+        this.loggedLinearVelocity = new LoggedDouble(this.name + "/LinearVelocity", 0.005);
+        this.loggedLinearVelocityError = new LoggedDouble(this.name + "/LinearVelocityError", 0.005);
         return this;
     }
 
@@ -183,10 +195,11 @@ public abstract class Mechanism<T extends MotorIO> {
 
         // If a radius has been configured, also log linear equivalents of position/velocity.
         if (radiusMeters != null) {
-            Logger.recordOutput(this.name + "/LinearPosition", getLinearPosition());
-            Logger.recordOutput(this.name + "/LinearPositionError", getLinearPositionError());
-            Logger.recordOutput(this.name + "/LinearVelocity", getLinearVelocity());
-            Logger.recordOutput(this.name + "/LinearVelocityError", getLinearVelocityError());
+            // Use LoggedDouble to only write when values change to reduce NT traffic
+            loggedLinearPosition.log(getLinearPosition().in(Meters));
+            loggedLinearPositionError.log(getLinearPositionError().in(Meters));
+            loggedLinearVelocity.log(getLinearVelocity().in(MetersPerSecond));
+            loggedLinearVelocityError.log(getLinearVelocityError().in(MetersPerSecond));
         }
     }
 
---
+++ src/main/java/frc/lib/util/LoggedDouble.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2026 Windham Windup
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <https://www.gnu.org/licenses/>.
+ */
+package frc.lib.util;
+
+import org.littletonrobotics.junction.Logger;
+
+/** Helper that records a double to the logger only when it changes beyond an absolute tolerance. */
+public class LoggedDouble {
+    private final String key;
+    private final double tolerance;
+    private double last;
+    private boolean hasLast = false;
+
+    /** No tolerance (exact change required). */
+    public LoggedDouble(String key) {
+        this(key, 0.0);
+    }
+
+    /**
+     * Absolute tolerance only. Value is logged when |value - last| > tolerance.
+     *
+     * @param key logging key
+     * @param tolerance minimum absolute delta required to trigger a log
+     */
+    public LoggedDouble(String key, double tolerance) {
+        this.key = key;
+        this.tolerance = Math.abs(tolerance);
+    }
+
+    /** Log the value if it differs from the last logged value by more than the tolerance. */
+    public void log(double value) {
+        if (!hasLast) {
+            last = value;
+            hasLast = true;
+            Logger.recordOutput(key, value);
+            return;
+        }
+
+        double delta = Math.abs(value - last);
+        if (delta > tolerance) {
+            last = value;
+            Logger.recordOutput(key, value);
+        }
+    }
+
+    /** Force a log regardless of previous value. */
+    public void force(double value) {
+        last = value;
+        hasLast = true;
+        Logger.recordOutput(key, value);
+    }
+
+    /** Get last recorded value (may be null). */
+    public Double getLast() {
+        return hasLast ? Double.valueOf(last) : null;
+    }
+}
