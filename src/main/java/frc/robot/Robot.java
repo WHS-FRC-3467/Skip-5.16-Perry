e6043e78227269dcaf0c4e104b4ce00bf05737f3
cabnit-rs <jkistler28@windhamsd.org>
2026-04-21 20:06:25 -0400

    added autos

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -111,10 +111,10 @@ public class Robot extends LoggedRobot {
                     || constants.SteerMotorType != SteerMotorArrangement.TalonFX_Integrated) {
                 throw new RuntimeException(
                         "You are using an unsupported swerve configuration, which this template"
-                            + " does not support without manual customization. The 2025 release of"
-                            + " Phoenix supports some swerve configurations which were not"
-                            + " available during 2025 beta testing, preventing any development and"
-                            + " support from the AdvantageKit developers.");
+                                + " does not support without manual customization. The 2025 release of"
+                                + " Phoenix supports some swerve configurations which were not"
+                                + " available during 2025 beta testing, preventing any development and"
+                                + " support from the AdvantageKit developers.");
             }
         }
 
